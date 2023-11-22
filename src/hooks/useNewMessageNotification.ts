eae1aa4776bfda309bd57dd10ad34b92c4e20f9c
baha-a <bhlshrf@gmail.com>
2023-11-22 09:39:34 +0300

    Clean ups

--- src/hooks/useNewMessageNotification.ts
+++ src/hooks/useNewMessageNotification.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../store';
 import { resetNewMessagesAmount } from '../slices/chats.slice';
